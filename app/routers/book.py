ae4634e222ab49139f673b78b8b8766ea27f7e09
settarovartur <settarovartur2@gamil.com>
2025-10-29 14:08:40 +0500

    init

--- app/routers/book.py
+++ app/routers/book.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, status
 from sqlalchemy.orm import Session
 
 
@@ -23,9 +23,57 @@ async def get_books(
     return Books
 
 
-@router.get("/{b}")
+@router.get('/filter')
+async def filter_books(
+    min :int,
+    max :int,
+    db:Session= Depends(get_db)):
+
+     result = db.query(Book).filter(Book.year.between(min, max)).all()
+     return result
+
+@router.get("/{book_id}", response_model=BookOut)
+async def get_book_by_id(
+    book_id: int = Path(ge = 1),
+    db:Session= Depends(get_db)):
+    book = db.query(Book).filter(Book.id == book_id).first()
+
+    if not book:
+        raise HTTPException(status_code=404, 
+        detail=f"Book with id {book_id} not found")
+    return book
 
 
 
+@router.post('', response_model=BookOut, status_code=status.HTTP_201_CREATED)
+async def create_book(
+    book_data: BookCreate,
+    db: Session = Depends(get_db),
+):
+    # Kitob mavjudligini tekshirish
+    book = db.query(Book).filter(
+        Book.title == book_data.title,
+        Book.author == book_data.author,
+        Book.year == book_data.year
+    ).first()
 
-
+    if book:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Book already exists"
+        )
+    
+    # Yangi kitob yaratish
+    new_book = Book(
+        title=book_data.title,
+        author=book_data.author,
+        genre=book_data.genre.value,  # Agar Enum bo'lsa
+        year=book_data.year,
+        rating=book_data.rating
+    )
+    
+    db.add(new_book)
+    db.commit()
+    db.refresh(new_book)
+    
+    return new_book
--- app/schemas/schemas.py
+++ app/schemas/schemas.py
@@ -14,3 +14,17 @@ class BookOut(BaseModel):
     
     class Config:
         from_attributes = True
+
+class BookCreate(BaseModel):
+    title:str = Field(min_length=3)
+    author : str = Field(min_length=3)
+    genre :Genre
+    year :int = Field(ge = 1)
+    rating :float = Field(ge=0,le=5)
+
+class BookUpdate(BaseModel):
+    title:Annotated[str|None,Field(min_length=3)] = None
+    author : Annotated[str|None,Field(min_length=3)] = None
+    genre :Genre | None = None
+    year :Annotated[int|None,Field(ge = 1)] = None
+    rating :Annotated[float|None,Field(ge=0,le=5)] = None
